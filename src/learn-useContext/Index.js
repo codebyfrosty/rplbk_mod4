acd908b18b112dbbf85979a65231efe19d625b38
Hilmi Laptop <57535737+codebyfrosty@users.noreply.github.com>
2022-11-02 23:05:33 +0700

    Update Counter.js and useContext

--- src/learn-useContext/Index.js
+++ src/learn-useContext/Index.js
@@ -1,48 +1,80 @@
-import React, {useState,useContext} from "react";
+import React, { useState, useContext } from "react";
 import "./Index.css";
-const themes ={
-light: {
-id:1,
-foreground: "#000000",
-background: "#eeeeee",
+const themes = {
+    light: {
+        id: 1,
+        foreground: "#000000",
+        background: "#eeeeee",
 
-},
-dark: {
-id:2,
-foreground: "#ffffff",
-background: "#222222",
-},
+    },
+    dark: {
+        id: 2,
+        foreground: "#ffffff",
+        background: "#222222",
+    },
 }
+
+export const anggota = {
+    hilmi: {
+        id: 1,
+        nama: "Hilmi Ahmad",
+        nim: "21120119130048",   
+    },
+
+    lukman: {
+        id: 2,
+        nama: "Lukman Ernandi",
+        nim: "21120119130049",
+    },
+
+    peggy: {
+        id: 3,
+        nama: "Peggy Raihannisa Zakiyyahannas",
+        nim: "21120119130111",
+    },
+
+    haickal: {
+        id: 4,
+        nama: "Haickal Fattih Pratama",
+        nim: "21120119140131",
+    }
+}
+
+
 const ThemeContext = React.createContext(themes.light);
-export default function Index(){
-const [theme,setTheme] = useState(themes.dark);
-const changeTheme = () => {
-if (theme.id === themes.light.id) {
-setTheme(themes.dark);
-} else {
-setTheme(themes.light);
-}
-};
-return(
-<ThemeContext.Provider value={{theme,changeTheme}}>
-<div className="Main" style={{background:
-theme.background,color: theme.foreground}}>
-<p className="Text">Theme by useContext</p>
-<p>KELOMPOK04</p>
-<ThemedButton/>
-</div>
-</ThemeContext.Provider>
-);
+export default function Index() {
+    const [theme, setTheme] = useState(themes.dark);
+    const changeTheme = () => {
+        if (theme.id === themes.light.id) {
+            setTheme(themes.dark);
+        } else {
+            setTheme(themes.light);
+        }
+    };
+    return (
+        <ThemeContext.Provider value={{ theme, changeTheme }}>
+            <div className="Main" style={{
+                background:
+                    theme.background, color: theme.foreground
+            }}>
+                <p className="Text">Theme by useContext</p>
+                <p>KELOMPOK04</p>
+                <ThemedButton />
+            </div>
+        </ThemeContext.Provider>
+    );
 }
 function ThemedButton() {
-const {theme,changeTheme} = useContext(ThemeContext);
-return(
-<button
-className="Button"
-style={{background: theme.background, color:
-theme.foreground}}
-onClick={changeTheme}>
-i am styled by theme context!
-</button>
-);
+    const { theme, changeTheme } = useContext(ThemeContext);
+    return (
+        <button
+            className="Button"
+            style={{
+                background: theme.background, color:
+                    theme.foreground
+            }}
+            onClick={changeTheme}>
+            i am styled by theme context!
+        </button>
+    );
 }
--- src/learn-useState/Counter.js
+++ src/learn-useState/Counter.js
@@ -1,23 +1,59 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import logo from "../logo.svg";
 import "./Counter.css";
+import { anggota } from "../learn-useContext/Index.js";
+
+
+
+const AnggotaContext = React.createContext(anggota.hilmi);
 
 export default function Counter() {
     const [inputValue, setInputValue] = useState('')
     const [showImage, setShowImage] = useState(true);
     const [count, setCount] = useState(0);
+    const [teks, setTeks] = useState('');
+    //buat useContext
+    const [angkel, setAnggota] = useState(anggota.hilmi);
+
+
     const countUp = () => {
         setCount(count + 1);
-
+        probAnggota();
     };
     const countDown = () => {
         setCount(count - 1);
+        probAnggota();
+
     };
     const hideImage = () => {
         setShowImage(!showImage);
     };
+
     const inputHandler = (event) => {
         setInputValue(event.target.value)
+    }
+
+    const probAnggota = () => {
+        if (count % 4 === 3) {
+            setAnggota(anggota.haickal);
+
+        }
+
+        if (count % 4 === 2) {
+            setAnggota(anggota.peggy);
+
+        }
+
+        if (count % 4 === 1) {
+            setAnggota(anggota.lukman);
+
+        }
+
+        if (count % 4 === 0) {
+            setAnggota(anggota.hilmi);
+
+        }
+
     }
 
     return (
@@ -25,8 +61,9 @@ export default function Counter() {
             <p className="Text">Counter with useState</p>
             <input type='text' onChange={inputHandler}/>
             <p>{ inputValue }</p>
-            <p>KELOMPOK 4</p>
+            <p>KELOMPOK 04</p>
             
+
             <div className="ViewImage">
                 <div className="ViewImage1">
                     <img className="App-logo"
@@ -42,6 +79,15 @@ export default function Counter() {
                 </button>
             </div>
             <p className="Text">{count}</p>
+            <p>Data dari useContext</p>
+
+            <AnggotaContext.Provider value={{ angkel, probAnggota }}>
+                <div className="predict">
+                    <AnggotaCard />
+                </div>
+            </AnggotaContext.Provider>
+
+
             <div className="ViewButton">
                 <div className="ViewButton2">
                     <button className="Button" onClick={countUp}>
@@ -54,6 +100,23 @@ export default function Counter() {
                     </button>
                 </div>
             </div>
+
+
+
         </div>
     );
 }
+
+
+
+function AnggotaCard() {
+    const { angkel, probAnggota } = useContext(AnggotaContext);
+    return (
+        <div >
+            <h2 style={{ textAlign: "center", }}> Nama:{angkel.nama} </h2>
+            <h2 style={{ textAlign: "center", }}> NIM: {angkel.nim}</h2>
+        </div>
+
+    )
+
+} 
