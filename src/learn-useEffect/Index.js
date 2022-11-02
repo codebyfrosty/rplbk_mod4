cdbbcfe33e8eb6e367504a21b1c3c83da4d4feed
Peggy25 <64076424+Peggy25@users.noreply.github.com>
2022-11-02 22:05:19 +0700

    Update Index.js

--- src/learn-useEffect/Index.js
+++ src/learn-useEffect/Index.js
@@ -2,60 +2,88 @@ import React, { useState, useEffect } from "react";
 import "./Index.css";
 export default function Index() {
     const [count, setCount] = useState(0);
-    const [data, setData] = useState([]);
+    // const [data, setData] = useState([]);
+    const [message, setMessage]=useState('Ayo Tebak ada berapa');
     //dijalankan 1 kali
-    useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/todos')
-            .then((response) => response.json())
-            .then((data) => {
-                console.log(data);
-                setData(data);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }, []);
+    // useEffect(() => {
+    //     fetch('https://jsonplaceholder.typicode.com/todos')
+    //         .then((response) => response.json())
+    //         .then((data) => {
+    //             console.log(data);
+    //             setData(data);
+    //         })
+    //         .catch((error) => {
+    //             console.log(error);
+    //         });
+    // }, []);
     //dijalankan terus setiap ada perubahan count
-    useEffect(() => {
-        if (count > 0) {
-            alert('component will update & count ${count}');
-        }
-    }, [count]);
+    // useEffect(() => {
+    //     if (count > 0) {
+    //         alert('component will update & count ${count}');
+    //     }
+    // }, [count]);
     //dijalankan terus menerus
-    useEffect(() => {
-        console.log('spam console kuy');
-    });
+    // useEffect(() => {
+    //     console.log('spam console kuy');
+    // });
     const countUp = () => {
         setCount(count + 1);
     };
     const countDown = () => {
         setCount(count - 1);
     };
+
+    useEffect(() => {
+        if (count==12){
+            setMessage("Yak Bener")
+        }
+    
+        
+        else if (count<0){
+            setMessage("Yakali masa fakultasnya mines")
+        }
+        
+        else if (count>12){
+            setMessage("Kelebihan gan")
+        }
+
+        else if (count>0){
+            setMessage("Kurang gan")
+        }
+        
+    
+      });
+
+
     return (
         <div className="Main">
             <p className="Text"> Learn useEffect</p>
             <p>KELOMPOK04</p>
-            <ul>
+            {/* <ul>
                 {data.slice(0, 10).map((value) => (
                     <li key={value.id}>{value.title}</li>
                 ))}
-            </ul>
+            </ul> */}
+            <h1>Coba Tebak ada berapa fakultas di Undip</h1>
             <p className="Text">{count}</p>
             <div className="ViewButton">
 
                 <div className="ViewButton2">
                     <button className="Button"
-                        onClick={countUp}>
-                        Up
+                        onClick={countDown}>
+                        Kurang
                     </button>
                 </div>
                 <div className="ViewButton1">
                     <button className="Button"
-                        onClick={countDown}>
-                        Down
+                        onClick={countUp}>
+                        Tambah
                     </button>
+                    
                 </div>
+                
             </div>
+            <h1>{message}</h1>
         </div>
     );
 }
